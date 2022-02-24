0ce254908cb8487e25afaaf1b3c526c563780dbf
sumeetkhanna <sumeet.khanna@wsp.com>
2022-02-24 20:37:13 +0530

    string manipulation test - functional way

--- Modules/Module1/StringTests.js
+++ Modules/Module1/StringTests.js
@@ -4,7 +4,7 @@ splitTrim = CT.Utils.compose(CT.Utils.map(CT.StringUtils.trim), CT.StringUtils.s
 const splitTrimStrings = CT.Utils.curry((func, delimiter, value) => func(delimiter, value))
 const splitTrimBlocks = splitTrimStrings(splitTrim, '+')
 const splitTrimVectors = splitTrimStrings(splitTrim, '/')
-const splitTrimSetValue = CT.Utils.compose(CT.Utils.mapFirstN(splitTrimStrings(splitTrim, ':'), 1))
+const splitTrimSetValue = CT.Utils.mapAt(splitTrimStrings(splitTrim, ':'), 0)
 
 const CalcNofDec = (value) => {
     const fValue = Math.abs(Number(value))
@@ -68,3 +68,37 @@ function formatValue (str) {
 
     console.log(`formatted string: ${execute(str)}`)
 }
+
+// const validateSetValues = CT.Utils.curry((index, values) => {
+//     const evaluate = CT.Utils.compose(
+//         CT.Utils.ifSomething(formatCycleTime, 3),
+//         CT.Utils.ifSomething(formatCycleTime, 2),
+//         CT.Utils.ifSomething(formatSetValue, 1),
+//         CT.Utils.ifSomething(formatSetValue, 0))
+//     const execute = chain(evaluate)
+//     values[index] = execute(values[index])
+//     return values
+// })
+//
+// function validateValue (str) {
+//     console.log(`Non formatted string: ${str}`)
+//
+//     const splitTrim = CT.Utils.compose(
+//         CT.Utils.map(splitTrimSetValue),
+//         CT.Utils.map(splitTrimVectors),
+//         splitTrimBlocks)
+//
+//     const evaluate = chain(CT.Utils.compose(
+//         CT.Utils.join(' / '),
+//         CT.Utils.ifSomething(formatCycleInterval, 3),
+//         CT.Utils.ifSomething(formatRampRate, 2),
+//         CT.Utils.ifSomething(formatStartTime, 1),
+//         formatSetValues(false, 0)))
+//
+//     const execute = chain(CT.Utils.compose(
+//         CT.Utils.join(' + '),
+//         CT.Utils.map(evaluate),
+//         splitTrim))
+//
+//     console.log(`formatted string: ${execute(str)}`)
+//}
