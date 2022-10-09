fc6d1dadcfb4a1c57fafa54cde950f6f6ab8d1b2
sumeetkhanna <sumeet.khanna@wsp.com>
2022-10-09 19:29:12 +0530

    can actions for grid, remove unwanted files

--- Modules/age/ViewController.js
+++ Modules/age/ViewController.js
@@ -74,21 +74,29 @@ const ViewController = function (viewModel) {
         viewModel.set('addRow', function () {
             const index = g.addRow(CELL_INSERTION_POSITION.end, 'ageGridId')
             pushAddRemoveCommand(index)
+            this.set('canremoveRow', g.hasData('ageGridId'))
         })
 
         viewModel.set('addBeforeRow', function () {
             const index = g.addRow(CELL_INSERTION_POSITION.before, 'ageGridId')
             pushAddRemoveCommand(index)
+            this.set('canremoveRow', g.hasData('ageGridId'))
         })
 
         viewModel.set('addAfterRow', function () {
             const index = g.addRow(CELL_INSERTION_POSITION.after, 'ageGridId')
             pushAddRemoveCommand(index)
+            this.set('canremoveRow', g.hasData('ageGridId'))
         })
 
         viewModel.set('removeRow', function () {
             CT.GridUtils.removeRow(CELL_INSERTION_POSITION.end, 'ageGridId')
+            this.set('canremoveRow', g.hasData('ageGridId'))
         })
+
+        viewModel.set('canaddRow', true)
+
+        viewModel.set('canremoveRow', g.hasData('ageGridId'))
 
         // TODO_SK get rid of this, needs this strangely to execute bind commands
         viewModel.set('apply', function () {
