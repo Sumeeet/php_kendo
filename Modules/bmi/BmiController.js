4599aee492245f1c07ca2cddf8cbb0b0d9eb2436
sumeetkhanna <sumeet.khanna@wsp.com>
2022-11-08 19:36:22 +0530

    run static analysis - eslint

--- Modules/bmi/BmiController.js
+++ Modules/bmi/BmiController.js
@@ -1,102 +1,124 @@
-'use strict'
-const undoRedo = new UndoRedo()
+"use strict";
+const undoRedo = new UndoRedo();
 const BmiController = function (viewModel) {
-    const v = CT.Validations
-    const u = CT.Utils
+  const v = CT.Validations;
+  const u = CT.Utils;
 
-    const ageEdit = getElement('ageInputId')
-    const heightEdit = getElement('heightInputId')
-    const weightEdit = getElement('weightInputId')
+  const ageEdit = getElement("ageInputId");
+  const heightEdit = getElement("heightInputId");
+  const weightEdit = getElement("weightInputId");
 
-    // const debounce = CT.Decorators.debounce(addCommands, 400);
+  // const debounce = CT.Decorators.debounce(addCommands, 400);
 
-    function bindDependencies (viewModel) {
-        let bmiMapper = BmiMapper()
+  function bindDependencies(viewModel) {
+    let bmiMapper = BmiMapper();
 
-        // update bmi value when height or weight is changed
-        viewModel.set('bmi',function() {
-            return bmiMapper.calculateBmi(this.get('weight.value'), this.get('height.value'))
+    // update bmi value when height or weight is changed
+    viewModel.set("bmi", function () {
+      return bmiMapper.calculateBmi(
+        this.get("weight.value"),
+        this.get("height.value")
+      );
+    });
+
+    // initialize bmi grid
+    //const dataSource = { dataSource: { data: {}}, columns: {}, width: 1500, height: 830, editable: "incell" }
+    // dataSource.dataSource.data = bmiMapper.getBmiGridData()
+    viewModel.set("bmiGrid", bmiMapper.getBmiGridData());
+
+    // dataSource.columns = bmiMapper.getBmiColumnInfo()
+    //$('#gridId').kendoGrid(dataSource)
+    //var grid = $("#grid").data("kendoGrid");
+    //grid.editCell($("#grid td:eq(0)"));
+  }
+
+  function bindCommands(viewModel) {
+    viewModel.set("undo", function () {
+      undoRedo.undo("age.value");
+    });
+
+    viewModel.set("redo", function () {
+      undoRedo.redo("age.value");
+    });
+
+    // TODO_SK get rid of this, needs this strangely to call undo/redo commands
+    viewModel.set("apply", function () {});
+  }
+
+  (function () {
+    viewModel.registerValidations(
+      "age.value",
+      [
+        v.isInRange(viewModel.get("age.min"), viewModel.get("age.max")),
+        v.isPositive("Age must be a positive number"),
+        v.isNumber("Please enter a valid number"),
+      ],
+      u.updateError("errorId1")
+    );
+
+    viewModel.registerValidations(
+      "height.value",
+      [
+        v.isInRange(viewModel.get("height.min"), viewModel.get("height.max")),
+        v.isPositive("Height must be a positive number"),
+        v.isNumber("Please enter a valid number"),
+      ],
+      u.updateError("errorId2")
+    );
+
+    viewModel.registerValidations(
+      "weight.value",
+      [
+        v.isInRange(viewModel.get("weight.min"), viewModel.get("weight.max")),
+        v.isPositive("Weight must be a positive number"),
+        v.isNumber("Please enter a valid number"),
+      ],
+      u.updateError("errorId3")
+    );
+
+    // This is calculated field, so limits are defined manually, but
+    // it can be pulled from other source too.
+    viewModel.registerValidations(
+      "bmi",
+      [
+        v.isInRange(12, 42),
+        v.isPositive("bmi must be a positive number"),
+        v.isNumber("Please enter a valid number"),
+      ],
+      u.updateError("errorId4")
+    );
+
+    ageEdit.addEventListener("keyup", (event) => {
+      addCommands(event, "age.value", ageEdit.value);
+    });
+
+    heightEdit.addEventListener("keyup", (event) => {
+      addCommands(event, "height.value", heightEdit.value);
+    });
+
+    weightEdit.addEventListener("keyup", (event) => {
+      addCommands(event, "weight.value", weightEdit.value);
+    });
+
+    bindDependencies(viewModel);
+
+    bindCommands(viewModel);
+  })();
+
+  function addCommands(event, prop, value) {
+    if (event.key === "Enter") {
+      undoRedo.push(
+        prop,
+        new EditCommand(viewModel, function (readCache) {
+          this.set(prop, readCache ? null : value);
         })
-
-        // initialize bmi grid
-        //const dataSource = { dataSource: { data: {}}, columns: {}, width: 1500, height: 830, editable: "incell" }
-        // dataSource.dataSource.data = bmiMapper.getBmiGridData()
-        viewModel.set('bmiGrid', bmiMapper.getBmiGridData())
-
-        // dataSource.columns = bmiMapper.getBmiColumnInfo()
-        //$('#gridId').kendoGrid(dataSource)
-        //var grid = $("#grid").data("kendoGrid");
-        //grid.editCell($("#grid td:eq(0)"));
+      );
     }
+  }
 
-    function bindCommands (viewModel) {
-        viewModel.set('undo', function () {
-            undoRedo.undo('age.value')
-        })
+  function reset() {
+    undoRedo.clear();
+  }
 
-        viewModel.set('redo', function () {
-            undoRedo.redo('age.value')
-        })
-
-        // TODO_SK get rid of this, needs this strangely to call undo/redo commands
-        viewModel.set('apply', function () {})
-    }
-
-    (function() {
-        viewModel.registerValidations('age.value',
-            [v.isInRange(viewModel.get('age.min'), viewModel.get('age.max')),
-                v.isPositive('Age must be a positive number'),
-                v.isNumber('Please enter a valid number')],
-            u.updateError("errorId1"));
-
-        viewModel.registerValidations('height.value',
-            [v.isInRange(viewModel.get('height.min'), viewModel.get('height.max')),
-                v.isPositive('Height must be a positive number'),
-                v.isNumber('Please enter a valid number')],
-            u.updateError("errorId2"));
-
-        viewModel.registerValidations('weight.value',
-            [v.isInRange(viewModel.get('weight.min'), viewModel.get('weight.max')),
-                v.isPositive('Weight must be a positive number'),
-                v.isNumber('Please enter a valid number')],
-            u.updateError("errorId3"));
-
-        // This is calculated field, so limits are defined manually, but
-        // it can be pulled from other source too.
-        viewModel.registerValidations('bmi',
-            [v.isInRange(12, 42),
-                v.isPositive('bmi must be a positive number'),
-                v.isNumber('Please enter a valid number')],
-            u.updateError("errorId4"));
-
-        ageEdit.addEventListener('keyup', (event) => {
-            addCommands(event, 'age.value', ageEdit.value);
-        })
-
-        heightEdit.addEventListener('keyup', (event) => {
-            addCommands(event, 'height.value', heightEdit.value);
-        })
-
-        weightEdit.addEventListener('keyup', (event) => {
-            addCommands(event, 'weight.value', weightEdit.value);
-        })
-
-        bindDependencies(viewModel)
-
-        bindCommands(viewModel)
-    })()
-
-    function addCommands(event, prop, value) {
-        if (event.key === 'Enter') {
-            undoRedo.push(prop, new EditCommand(viewModel, function (readCache) {
-                this.set(prop, readCache ? null : value)
-            }))
-        }
-    }
-
-    function reset() {
-        undoRedo.clear()
-    }
-
-    return { reset }
-}
+  return { reset };
+};
--- Modules/bmi/BmiMapper.js
+++ Modules/bmi/BmiMapper.js
@@ -1,56 +1,64 @@
-'use strict'
+"use strict";
 
 const BmiMapper = () => {
+  const heightInCms = [
+    142.2, 144.7, 147.3, 149.8, 152.4, 154.9, 157.4, 160.0, 162.5, 165.1, 167.6,
+    170.1, 172.7, 175.2, 177.8, 180.3, 182.8, 185.4, 187.9, 190.5, 193.0, 195.5,
+    198.1, 200.6, 203.2, 205.7, 208.2, 210.8,
+  ];
 
-    const heightInCms = [142.2, 144.7, 147.3, 149.8, 152.4, 154.9, 157.4, 160.0, 162.5,
-        165.1, 167.6, 170.1, 172.7, 175.2, 177.8, 180.3, 182.8, 185.4, 187.9, 190.5,
-        193.0, 195.5, 198.1, 200.6, 203.2, 205.7, 208.2, 210.8]
+  let heightInFtInch = [];
 
-    let heightInFtInch = []
+  // in kgs
+  const weightInKgs = [
+    41, 45, 50, 54, 59, 64, 68, 73, 77, 82, 86, 91, 95, 100, 104, 109, 113, 118,
+    122, 127, 132,
+  ];
 
-    // in kgs
-    const weightInKgs = [41, 45, 50, 54, 59, 64, 68, 73, 77, 82, 86, 91, 95, 100, 104,
-        109, 113, 118, 122, 127, 132]
+  let weightInLbs = [];
 
-    let weightInLbs = []
+  let bmiTable = [];
 
-    let bmiTable = []
+  const calculateBmi = (weight, height) =>
+    Math.round((weight * 10000) / (height * height));
 
-    const calculateBmi = (weight, height) => Math.round((weight * 10000) / (height * height));
+  const cmsToInches = (height) => {
+    const heightInches = height / 2.54;
+    const heightInFeet = Math.floor(heightInches / 12);
+    const factorInc = heightInches - 12 * heightInFeet;
+    return `${heightInFeet}' ${
+      Math.round((factorInc + Number.EPSILON) * 10) / 10
+    }''`;
+  };
 
-    const cmsToInches = (height) => {
-        const heightInches = height / 2.54
-        const heightInFeet = Math.floor(heightInches / 12)
-        const factorInc = heightInches - 12 * heightInFeet
-        return `${heightInFeet}' ${Math.round((factorInc + Number.EPSILON) * 10) / 10}''`
-    }
+  (() => {
+    heightInFtInch = heightInCms.map((h) => cmsToInches(h));
+    weightInLbs = weightInKgs.map((w) => Math.round(w * 2.20462));
 
-    (() => {
-        heightInFtInch = heightInCms.map(h => cmsToInches(h))
-        weightInLbs = weightInKgs.map(w => Math.round(w * 2.20462))
+    bmiTable = heightInCms.map((h) =>
+      weightInKgs.map((w) => calculateBmi(w, h))
+    );
+    bmiTable.forEach((bmiRow, idx) => bmiRow.splice(0, 0, heightInCms[idx]));
+    bmiTable.forEach((bmiRow, idx) => bmiRow.splice(1, 0, heightInFtInch[idx]));
 
-        bmiTable = heightInCms.map(h => weightInKgs.map(w => calculateBmi(w, h)))
-        bmiTable.forEach((bmiRow, idx) => bmiRow.splice(0, 0, heightInCms[idx]))
-        bmiTable.forEach((bmiRow, idx) => bmiRow.splice(1, 0, heightInFtInch[idx]))
+    bmiTable.splice(0, 0, [" ", " ", ...weightInLbs]);
+    bmiTable.splice(1, 0, [" ", " ", ...weightInKgs]);
+    bmiTable.splice(2, 0, Array(23).fill(" "));
 
-        bmiTable.splice(0, 0, [' ', ' ', ...weightInLbs])
-        bmiTable.splice(1, 0, [' ', ' ', ...weightInKgs])
-        bmiTable.splice(2, 0, Array(23).fill(' '))
+    bmiTable[0][1] = "lbs";
+    bmiTable[1][1] = "kg";
+    bmiTable[2][0] = "cm";
+    bmiTable[2][1] = "ft/in";
+  })();
 
-        bmiTable[0][1] = 'lbs'
-        bmiTable[1][1] = 'kg'
-        bmiTable[2][0] = 'cm'
-        bmiTable[2][1] = 'ft/in'
-    })()
+  // const getBmiColumnInfo = () => weight.map((w, i) => {
+  //     const colName = `c${i}`
+  //     return { field: colName, title: colName, width: '30px'}
+  // })
 
-    // const getBmiColumnInfo = () => weight.map((w, i) => {
-    //     const colName = `c${i}`
-    //     return { field: colName, title: colName, width: '30px'}
-    // })
+  const getBmiGridData = () => {
+    return CT.GridUtils.populate(bmiTable);
+  };
 
-    const getBmiGridData = () => {
-        return CT.GridUtils.populate(bmiTable)
-    }
-
-    return { getBmiGridData, calculateBmi }
-}
+  return { getBmiGridData, calculateBmi };
+};
